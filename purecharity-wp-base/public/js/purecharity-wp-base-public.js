fd14f4657c933e8a152cfb4e8b94a0765be4a226
Rafael Dalprá <thedalpra@gmail.com>
2015-06-23 17:35:51 -0300

    Iframe option removed

--- purecharity-wp-base/public/js/purecharity-wp-base-public.js
+++ purecharity-wp-base/public/js/purecharity-wp-base-public.js
@@ -1,29 +1,4 @@
 (function( jQuery ) {
 	'use strict';
 
-  // Iframe specifics: Buttons
-  jQuery(document).on('click', '.iframe-button', function(){
-    if(jQuery(window).outerWidth() > 700){
-      jQuery('.pc-backdrop').show();
-      jQuery('.pc-backdrop').find('iframe').html('');
-      jQuery('.pc-backdrop').find('iframe').attr('src', jQuery(this).attr('href'));
-      return false;
-    }
-  })
-
-  // Forms
-  jQuery(document).on('submit', '.iframe-form', function(){
-    if(jQuery(window).outerWidth() > 700){
-      jQuery('.pc-backdrop').find('iframe').html('');
-      jQuery('.pc-backdrop').find('iframe').attr('src', jQuery(this).attr('action')+'?'+jQuery(this).serialize());
-      jQuery('.pc-backdrop').show();
-      return false;
-    }
-  })
-
-  jQuery(document).on('click', '.pc-backdrop', function(){
-    jQuery(this).hide();
-  })
-  // Iframe specifics
-
 })( jQuery );
