f81c2bd0fe14fec47b7a010df0ee8f8c000515c9
Rafael Dalprá <thedalpra@gmail.com>
2015-12-02 12:40:14 -0200

    Added all scenarios

--- public/partials/purecharity-plugin-template.php
+++ public/partials/purecharity-plugin-template.php
@@ -18,7 +18,18 @@ Template Name: Custom Purecharity Template
               
               preg_match_all( '/'. $pattern .'/s', $content, $matches );
 
-              if( is_array( $matches ) && array_key_exists( 2, $matches ) && in_array( 'fundraisers', $matches[2] ) )
+              if( is_array( $matches ) && array_key_exists( 2, $matches ) 
+                  && (
+                    in_array( 'sponsorships', $matches[2] ) ||
+                    in_array( 'sponsorship', $matches[2] ) ||
+                    in_array( 'fundraisers', $matches[2] ) ||
+                    in_array( 'fundraiser', $matches[2] ) ||
+                    in_array( 'trips', $matches[2] ) ||
+                    in_array( 'trip', $matches[2] ) ||
+                    in_array( 'giving_circles', $matches[2] ) ||
+                    in_array( 'giving_circle', $matches[2] )
+                  )
+                )
               {
                 foreach ($matches[0] as $value) {
                   $value = wpautop( $value, true );
