90e52db2d790d418b0f6b4d90807f19fbbceaaa1
Oleksandr Zemlianoi <raaccoon@gmail.com>
2017-01-04 15:15:50 +0200

    deactivation function

--- includes/purecharity-wp-base-deactivator.class.php
+++ includes/purecharity-wp-base-deactivator.class.php
@@ -22,15 +22,43 @@
  */
 class Purecharity_Wp_Base_Deactivator {
 
-  /**
-   * Deactivates the Pure Base Plugin.
-   *
-   * Sets all the necessary settings for the plugin.
-   *
-   * @since    1.0.0
-   */
-	public static function deactivate() {
+    /**
+     * Deactivates the Pure Base Plugin.
+     *
+     * Sets all the necessary settings for the plugin.
+     *
+     * @since    1.0.0
+     */
+    public static function deactivate() {
+        require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+        
+        $current = get_option( 'active_plugins', [] );
+        $deactivate = [];
 
-	}
+        foreach ( $current as $v ) {
+            if ( self::is_pure_plugin( $v ) ) {
+                $deactivate[] = $v;
+            }
+        }
+        
+        deactivate_plugins( $deactivate, true );
+    }
+
+    public static function is_pure_plugin( $name ) {
+        $plugins = [
+            'purecharity-wp-trips.php',
+            'purecharity-wp-donations.php',
+            'purecharity-wp-fundraisers.php',
+            'purecharity-wp-sponsorships.php',
+            'purecharity-wp-givingcircles.php',
+        ];
+
+        foreach ( $plugins as $v ) {
+            if ( strpos( $name, $v ) !== false ) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
